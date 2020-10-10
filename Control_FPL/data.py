85096a415ac66fca2f8d0b993574da502cac096d
ajay prasadh <ajay.viswanathan@microsoft.com>
2020-10-10 14:43:22 +0100

    fixed bugs in data

--- Control_FPL/data.py
+++ Control_FPL/data.py
@@ -30,16 +30,17 @@ class Data:
         self.this_year_teams = pd.read_csv(f"{self.local_root}{self.year}/teams.csv")
         self.previous_year_teams = pd.read_csv(f"{self.local_root}{self.history_year}/teams.csv")
         self.teams = pd.merge(self.this_year_teams, self.previous_year_teams)
+        
+    async def get_latest_player_data(self, chance_of_playing_threshold = 0):
+        """Gets latest player data 
 
-    def download_latest_game_week_data(self):
-        if os.path.exists(self.local_game_week_data_path):
-            os.remove(self.local_game_week_data_path)
-        wget.download(self.remote_game_week_data_path,
-                      out=self.local_game_week_data_path)
-        wget.download()
-
-    async def get_latest_player_data(self):
-        latest_player_data = pd.DataFrame(columns=['name', 'position', 'now_cost', 'element'])
+        Args:
+            chance_of_playing_threshold: Chance that player will play in next 2 rounds
+        
+        Returns:
+            Dataframe containing latest information about player
+        """
+        latest_player_data = pd.DataFrame(columns=['name', 'position', 'now_cost', 'element', "chance_of_playing_this_round", "chance_of_playing_next_round"])
         position_map = {
             1: "Goalkeeper",
             2: "Defender",
@@ -48,52 +49,72 @@ class Data:
         }
         async with aiohttp.ClientSession() as session:
             fpl = FPL(session)
-            for i in range(1, 498):
+            for i in range(1, 600):
                 try :
                     player = await fpl.get_player(i)
                     name = player.first_name + " " + player.second_name
-                    latest_player_data.loc[i] = [name, position_map[player.element_type], float(player.now_cost), i]
+                    latest_player_data.loc[i] = [name, position_map[player.element_type], float(player.now_cost), i, player.chance_of_playing_this_round, player.chance_of_playing_next_round]
                 except ValueError:
                     print(f"player not found {i}")
-        latest_player_data.dropna(inplace=True)
+        latest_player_data.fillna(0, inplace=True)
+        latest_player_data = latest_player_data[latest_player_data["chance_of_playing_this_round"] >= chance_of_playing_threshold]
+        latest_player_data = latest_player_data[latest_player_data["chance_of_playing_next_round"] >= chance_of_playing_threshold]
         return latest_player_data
 
-    async def player_swap(self, players_out, players_in):
-        async with aiohttp.ClientSession() as session:
-            fpl = FPL(session)
-            await fpl.login(email=os.environ['email'], password=os.environ['password'])
-            user = await fpl.get_user(5645003)
-            # await user.transfer([players_out], [players_in], max_hit=100)
-            squad = await user.get_team()
-            print(squad)
+    def get_historical_player_data_by_feature(self, player_feature) -> pd.DataFrame:
+        """Gets historical data for given feature
 
-    def get_historical_data_by_feature(self, feature) -> pd.DataFrame:
-        '''
-            Returns latest history dataframe for a given feature
-        '''
+        Args:
+            player_feature:player attribute for which data is to be obtained
+        
+        Returns:
+            Dataframe of shape (N, L) where each row contains player attribute through the course of L FPL rounds
+        """
         game_weeks1 = pd.read_csv(self.local_history_data_path)[
-            ['name', feature, 'GW', 'minutes']]
+            ['name', player_feature, 'GW', 'minutes']]
         remove_digits = str.maketrans("", "", "0123456789")
         remove_underscore = str.maketrans("_", " ", "")
         game_weeks1['name'] = game_weeks1.apply(lambda x: x['name'].translate(
             remove_underscore).translate(remove_digits).strip(), axis=1)
         game_weeks2 = pd.read_csv(self.local_game_week_data_path)[
-            ['name', feature, 'GW', 'minutes']]
+            ['name', player_feature, 'GW', 'minutes']]
         game_weeks2["GW"] = game_weeks2["GW"] + 53
         game_weeks = pd.concat((game_weeks1, game_weeks2))
         game_weeks = game_weeks[game_weeks['minutes']
-                                > self.minute_threshold_per_game][["name", "GW", feature]]
-        historical_data = pd.pivot_table(index="name",columns="GW",values=feature,data=game_weeks).reset_index()
+                                > self.minute_threshold_per_game][["name", "GW", player_feature]]
+        historical_data = pd.pivot_table(index="name",columns="GW",values=player_feature,data=game_weeks).reset_index()
         historical_data.columns = ["name"] + list(range(1, len(historical_data.columns)))
         historical_data.fillna(0, inplace=True)
         return historical_data
 
+    def get_historical_player_data_by_feature_set(self, player_features) -> np.array:
+        """Gets historical data for given feature set
+
+        Args:
+            player_features: featureset for which player attributes are to be obtained
+        
+        Returns:
+            Numpy array of shape (N, D, L) where each row is a D * L matrix and 
+            each row of the matrix is a player dimension and each column is sequence step.
+        """
+        historical_player_data_by_feature_set = [self.get_historical_player_data_by_feature(player_feature).values for player_feature in player_features]
+        return np.array(historical_player_data_by_feature_set).transpose(1, 0, 2)
+
     def get_team_feature(self, feature):
+        """Gets performance of team along a certain attribute
+
+        Args:
+            feature: feature along which performance is to be obtained
+        
+        Returns:
+            Pandas dataframe with a team id column and of shape (number of teams * L) 
+            where L is sequence step and each entry describes performance of team along feature.
+        """
         a = pd.read_csv("./data/2019-20/teams.csv")
         b = pd.read_csv("./data/2020-21/teams.csv")
         teams = pd.merge(b, a, on=['id'], how='outer', suffixes=("", "_prev"))
         team_feature = pd.DataFrame()
-        for id in range(1, 20):
+        for id in range(1, 21):
             team = teams[teams["id"] == id]["name"].values[0]
             team_file_name = team.replace(" ", "_")
             team_history, team_current = None, None
@@ -117,13 +138,21 @@ class Data:
         return team_feature
 
     def get_opponent_feature(self, feature):
-        opponents = self.get_historical_data_by_feature("opponent_team")
+        """Gets opposition data faced by player along dimension
+
+        Args:
+            feature: performance descriptor of opposition team
+        
+        Returns:
+            Dataframe containing player names and a feature matrix of size (N * L)
+        """
+        opponents = self.get_historical_player_data_by_feature("opponent_team")
         team_feature = self.get_team_feature(feature)
         for i in range(opponents.shape[0]):
             for j in range(1, opponents.shape[1]):
-                if opponents.iloc[i, j] != 0:
-                    opponent = int(opponents.iloc[i, j]) - 2
-                    opponents.iloc[i, j] = team_feature.iloc[opponent, j-1]
+                opponent = int(opponents.iloc[i, j])
+                if opponent != 0:
+                    opponents.iloc[i, j] = team_feature.iloc[opponent-1, j-1]
         opponents.fillna(0, inplace=True)
         return opponents
 
@@ -131,17 +160,10 @@ class Data:
         '''
             Returns a 3d tensor of shape (Num_players, num_opposition_features, seq_length)
         '''
-        historical_player_data_by_feature_set = self.get_historical_data_by_feature_set(player_features)
+        historical_player_data_by_feature_set = self.get_historical_player_data_by_feature_set(player_features)
         historical_opponent_data_by_feature_set = [self.get_opponent_feature(opposition_feature) for opposition_feature in opposition_features]
         historical_opponent_data_by_feature_set  =  np.array(historical_opponent_data_by_feature_set).transpose(1, 0, 2)
         return np.concatenate((historical_player_data_by_feature_set, historical_opponent_data_by_feature_set), axis=1)
-
-    def get_historical_data_by_feature_set(self, features) -> np.array:
-        '''
-            Returns a 3D tensor of shape (Num_players, num_features, seq_length)
-        '''
-        historical_data_by_feature_set = [self.get_historical_data_by_feature(feature).values for feature in features]
-        return np.array(historical_data_by_feature_set).transpose(1, 0, 2)
 
     def get_training_data_tensor(self, x, window_width=5, num_features=7, batch_size=50, train_test_split = 0.3) -> (DataLoader, DataLoader):
         '''
@@ -183,15 +205,17 @@ class Data:
         '''
             Returns most recent feature set data
         '''
-        historical_data_by_feature_set =  self.get_historical_data_by_feature_set(features)
+        historical_data_by_feature_set =  self.get_historical_player_data_by_feature_set(features)
         recent_form_by_feature_set = historical_data_by_feature_set[:, :, -window_width:]
         player_names = historical_data_by_feature_set[:,:,0:1]
         return np.concatenate((player_names, recent_form_by_feature_set), axis = 2)
 
     async def get_current_squad(self) -> pd.DataFrame:
-        '''
-            Get latest squad
-        '''
+        """Gets current squad belonging to user
+
+        Returns:
+            Dataframe containing current FPL squad of user
+        """
         async with aiohttp.ClientSession() as session:
             fpl = FPL(session)
             await fpl.login(email=os.environ['email'], password=os.environ['password'])
@@ -217,14 +241,38 @@ class Data:
 
 if __name__ == "__main__":
     data = Data()
-    #data.download_latest_game_week_data()
-    #latest_player_data = asyncio.run(data.get_latest_player_data())
+    
+    
+    # latest_player_data = asyncio.run(data.get_latest_player_data(chance_of_playing_threshold=0))
+    # print(latest_player_data.shape)
+
+    
+    #player_feature = "total_points"
+    #player_data_by_feature = data.get_historical_player_data_by_feature(player_feature)
+    #print(player_data_by_feature.head())
+
+
+    #player_features = ['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded']
+    #player_data_by_featureset = data.get_historical_player_data_by_feature_set(player_features)
+    #assert(player_data_by_featureset.shape[1] == len(player_features))
+    #print(player_data_by_featureset.shape)
+
     #print(latest_player_data.head())
     # print(data.get_latest_game_week_data().columns)
     #print(data.get_historical_data_by_feature_set(['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded']).shape)
-    print(data.get_historical_player_opponent_data_by_feature_set(['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded'], ["npxG", "npxGA"]).shape)
+    #print(data.get_historical_player_opponent_data_by_feature_set(['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded'], ["npxG", "npxGA"]).shape)
     #print(data.get_recent_data_by_features(['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded']).shape)
     #get_recent_by_player_and_opposition_features(['total_points', 'yellow_cards', 'assists', 'ict_index', 'saves', 'goals_scored', 'goals_conceded'], ["npxG"])
+    
+    
+    team_feature = data.get_team_feature("npxG")
+    print(team_feature)
+
+    opponent_feature = data.get_opponent_feature("npxG")
+    print(opponent_feature)
+    opponent_feature_sample = opponent_feature[opponent_feature["name"] == "Bruno Miguel Borges Fernandes"]
+    print(opponent_feature_sample)
+    
+    
     #current_squad = asyncio.run(data.get_current_squad())
     #print(current_squad)
-    #asyncio.run(data.player_swap(303, 164))
---
+++ Control_FPL/get_latest_data.sh
@@ -0,0 +1,4 @@
+rm -rf data/
+cd ../../Fantasy-Premier-League/
+git pull origin master
+cp -r data ../Control/Control_FPL/
