86eb45065e200cf9a272546e0add9d6c6d2573fc
ajayrfhp <ajayrfhp1710@gmail.com>
2020-10-31 19:22:23 +0000

    cleaned data processor and added standaradized team name file

--- Control_FPL/data_processor.py
+++ Control_FPL/data_processor.py
@@ -15,23 +15,34 @@ from torch.utils.data import TensorDataset, DataLoader
 from player import Player
 from team import Team
 
+
+def get_normalized_team_names():
+    team_names = pd.read_csv("standard_teams.csv")
+    return team_names
+
 def get_all_player_features(player_feature_names):
-    game_weeks1 = pd.read_csv("./data/2019-20/gws/merged_gw.csv")[
+    team_names = get_normalized_team_names()
+    gameweek_data_2019 = pd.read_csv("./data/2019-20/gws/merged_gw.csv")[
         ['name', "GW", "minutes", "opponent_team"] + player_feature_names]
     remove_digits = str.maketrans("", "", "0123456789")
     remove_underscore = str.maketrans("_", " ", "")
-    game_weeks1['name'] = game_weeks1.apply(lambda x: x['name'].translate(
+    gameweek_data_2019['name'] =gameweek_data_2019.apply(lambda x: x['name'].translate(
         remove_underscore).translate(remove_digits).strip(), axis=1)
-    game_weeks2 = pd.read_csv("./data/2020-21/gws/merged_gw.csv")[
+    gameweek_data_2019 = pd.merge(gameweek_data_2019, team_names, left_on = ['opponent_team'], right_on=['id_2019'], how="left")
+
+
+    gameweek_data_2020 = pd.read_csv("./data/2020-21/gws/merged_gw.csv")[
         ['name', "GW", "minutes", "opponent_team"] + player_feature_names]
-    game_weeks2["GW"] = game_weeks2["GW"] + 53
-    game_weeks = pd.concat((game_weeks1, game_weeks2))
-    
-    all_player_features = game_weeks[["name", "opponent_team"] + player_feature_names]
+    gameweek_data_2020["GW"] = gameweek_data_2020["GW"] + 53
+    gameweek_data_2020 = pd.merge(gameweek_data_2020, team_names, left_on = ['opponent_team'], right_on=['id_2020'], how="left")
+    game_weeks = pd.concat((gameweek_data_2019, gameweek_data_2020))
+    game_weeks["opponent"] = game_weeks["normalized_team_name"]
+    all_player_features = game_weeks[["name", "opponent"] + player_feature_names]
     all_player_features.fillna(0, inplace=True)
     return all_player_features
 
-async def get_players(player_feature_names=["total_points", "ict_index"], visualize=False):
+
+async def get_players(player_feature_names=["total_points", "ict_index"], visualize=False, num_players=10):
     """Gets latest player data
     
     Returns:
@@ -41,15 +52,13 @@ async def get_players(player_feature_names=["total_points", "ict_index"], visual
     players = []
     latest_player_data = pd.DataFrame(columns=['name', 'position', 'now_cost', 'element', "chance_of_playing_this_round", "chance_of_playing_next_round"])
     all_player_features =  get_all_player_features(player_feature_names)
-    for i in range(1, 590):
+    for i in range(1, num_players):
         try:
             player_data = await fpl.get_player(i)
             name = player_data.first_name + " " + player_data.second_name
             integer_position = player_data.element_type
             latest_price = player_data.now_cost
             team = player_data.team
-            print(i)
-            
             player = Player(id=i, name=name, integer_position=integer_position, team=team, latest_price=latest_price, window=4, player_feature_names=player_feature_names)
             player_features = all_player_features[all_player_features["name"] == name].transpose().values[1:]
             player.player_features = player_features[1:]
@@ -61,26 +70,8 @@ async def get_players(player_feature_names=["total_points", "ict_index"], visual
             print(f"Player {i} does not exist")
     return players
 
-def get_team_features(name, team_features=["npxGA"]):
-    a = pd.read_csv("./data/2019-20/teams.csv")
-    b = pd.read_csv("./data/2020-21/teams.csv")
-    team_normalization = pd.DataFrame()
-    team_normalization["name"] = ["Arsenal" , "Aston Villa", "Bournemouth",  "Brighton",
-        "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds", "Leicester", "Liverpool",  "Man City", "Man Utd",
-        "Newcastle", "Norwich", "Sheffield Utd", "Southampton", "Spurs", "Watford", "West Brom", "West Ham", "Wolves" ]
-    team_normalization["normalized_team_names"] = ["Arsenal" , "Aston Villa", "Bournemouth",  "Brighton",
-        "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds", "Leicester", "Liverpool", "Manchester City", "Manchester United",
-        "Newcastle United", "Norwich", "Sheffield United", "Southampton", "Tottenham", "Watford", "West Bromwich Albion", "West Ham", "Wolverhampton Wanderers" ]
-    teams = pd.merge(b, a, on=['id'], how='outer', suffixes=("", "_prev"))
-    teams = pd.merge(teams, team_normalization, on = ['name'], how = 'outer')
-    teams['name'] = teams['normalized_team_names']
-    team_feature = pd.DataFrame()
-    
-    id = teams[teams["name"] == name]["id"].values[0]
-    if not teams[teams["id"] == id]["name"].shape[0]:
-        return pd.DataFrame()
-    team = teams[teams["id"] == id]["name"].values[0]
-    team_file_name = team.replace(" ", "_")
+def get_team_features(team_name, team_feature_names=["npxGA"]):
+    team_file_name = team_name.replace(" ", "_")
     team_history, team_current = None, None
 
     if os.path.exists(f"./data/2020-21/understat/understat_{team_file_name}.csv"):
@@ -89,30 +80,81 @@ def get_team_features(name, team_features=["npxGA"]):
         team_current["id"] = id
         team_current["index"] += 39
 
-        if os.path.exists(f"./data/2019-20/understat/understat_{team_file_name}.csv"):
-            team_history = pd.read_csv(f"./data/2019-20/understat/understat_{team_file_name}.csv")
-            team_history = team_history.reset_index()
-            team_history["id"] = id
-            team_history["index"] += 1
-            team_current = pd.concat((team_history, team_current))
+    if os.path.exists(f"./data/2019-20/understat/understat_{team_file_name}.csv"):
+        team_history = pd.read_csv(f"./data/2019-20/understat/understat_{team_file_name}.csv")
+        team_history = team_history.reset_index()
+        team_history["id"] = id
+        team_history["index"] += 1
 
-    features = team_current[["id"] + team_features].transpose().values
+    team_current = pd.concat((team_history, team_current))
+    features = team_current[["id"] + team_feature_names].transpose().values
     return features
 
-def get_teams(team_features, visualize=False):
+
+def get_teams(team_feature_names, visualize=False):
+    team_names = get_normalized_team_names()
     teams = []
-    for name in ["Arsenal" , "Aston Villa", "Bournemouth",  "Brighton",
-        "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Leeds", "Leicester", "Liverpool", "Manchester City", "Manchester United",
-        "Newcastle United", "Norwich", "Sheffield United", "Southampton", "Tottenham", "Watford", "West Bromwich Albion", "West Ham", "Wolverhampton Wanderers" ]:
-        features = get_team_features(name,team_features)
-        if features.shape[0]:
-            id = features[0,0]
-            team = Team(id=id, name=name, team_features=team_features, features=features[1:], window=4)
+    for team_name in team_names["normalized_team_name"].values:
+        team_features = get_team_features(team_name, team_feature_names)
+        if team_features.shape[0]:
+            id = team_features[0,0]
+            team = Team( name=team_name, team_feature_names=team_feature_names, team_features=team_features[1:], window=4)
             if visualize:
+                print(team.window)
                 team.visualize()
             teams.append(team)
     return teams
 
+def get_training_datasets(players, teams, window=4, batch_size=5):
+    player_features_array = []
+    opponent_features_array = []
+    total_points_array = []
+    for player in players:
+        player_features = player.player_features # ( D * L matrix)
+        opponents = player.opponents # (1 * L matrix)
+        
+        # Break (D*L) matrices into (num*D*window) where num = int( L / window)
+        num_windows = player_features.shape[1] // (window+1)
+        player_features = player_features[:,:(1+window) * num_windows]
+        opponents = opponents[:,:(1+window) * num_windows]
+        indices = np.arange(0, (1+window) * num_windows)
+        player_features = player_features.reshape((-1, player_features.shape[0], window+1))[:,:,:-1]
+        opponents = opponents.reshape((-1, 1, window+1))[:,0,-1]
+        indices = indices.reshape((-1, 1, window+1))[:, 0, -1]
+        total_points = player_features[:,0,-1]
+        opponent_features = []
+        for opponent, index in zip(opponents, indices):
+            opponent_team = [team for team in teams if team.name == opponent][0]
+            opponent_team_feature = opponent_team.team_features
+            if opponent_team_feature.shape[1] >= index:
+                opponent_team_correct_feature = opponent_team.team_features[:,(index-window):index]
+            else:
+                opponent_team_correct_feature = np.zeros((opponent_team_feature.shape[0], window))
+            opponent_features.append(opponent_team_correct_feature)
+            assert(opponent_team_correct_feature.shape[1] == window)
+        opponent_features = np.array(opponent_features)
+        
+        player_features_array.extend(player_features)
+        opponent_features_array.extend(opponent_features)
+        total_points_array.extend(total_points)
+        
+    player_features_array = torch.tensor(np.array(player_features_array).astype(float))
+    opponent_features_array = torch.tensor(np.array(opponent_features_array).astype(float))
+    total_points_array = torch.tensor(np.array(total_points_array).astype(float))
+    assert(player_features_array.shape[0] == total_points_array.shape[0])
+
+    indices = np.random.permutation(range(0, len(player_features_array)))
+    train_length = int(0.8 * len(indices))
+
+    train_player_features_array, test_player_features_array = player_features_array[indices[:train_length]], player_features_array[indices[train_length:]]
+    train_opponent_features_array, test_opponent_features_array = opponent_features_array[indices[:train_length]], opponent_features_array[indices[train_length:]]
+    train_total_points_array, test_total_points_array = total_points_array[indices[:train_length]], total_points_array[indices[train_length:]]
+    
+
+
+    train_loader = DataLoader(TensorDataset(train_player_features_array, train_opponent_features_array, train_total_points_array), batch_size=batch_size)
+    test_loader = DataLoader(TensorDataset(test_player_features_array, test_opponent_features_array, test_total_points_array), batch_size=batch_size)
+    return train_loader, test_loader
 
 async def get_fpl():
     async with aiohttp.ClientSession() as session:
@@ -121,6 +163,10 @@ async def get_fpl():
 
 if __name__ == "__main__":
     fpl = asyncio.run(get_fpl())
-    asyncio.run(get_players(["total_points", "ict_index"], visualize=True))
-    team_feature =  get_teams(team_features=["npxGA", "npxG", "scored", "xG","xGA","xpts"])
-    print(team_feature)
+    players = asyncio.run(get_players(["total_points", "ict_index", 'goals_scored', 'assists', 'clean_sheets', "goals_conceded", "saves"], visualize=False, num_players=600))
+    teams = get_teams(team_feature_names=["npxGA", "npxG", "scored", "xG","xGA","xpts"], visualize=False)
+    train_loader, test_loader = get_training_datasets(players, teams)
+    #print(players)
+    #print(teams)
+    #print(get_normalized_team_names())
+
